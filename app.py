3e38cf4355a248dc2b42579a4fd128bb95c6d7ae
Sairam Solake <sairamsolake21@gmail.com>
2026-05-05 14:39:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Infoorigin Team Welcome Happy Learninng...."
+    return "Hello Infoorigin Team Welcome Happy Learninng......"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
